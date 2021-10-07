41f0782113fb3149831cc19b21b552fc3bf9cf35
RickardKarl <rickard.karl97@gmail.com>
2021-10-07 09:33:41 +0200

    Added LaTeX table printout

--- src/plotutils.py
+++ src/plotutils.py
@@ -2,6 +2,7 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np 
+import pandas as pd
 
 from .base import Data
 
@@ -54,13 +55,20 @@ color_dict = {
     'Baseline' : 'blue',
     'LuPTS'    : 'red',
     'Stat-LuPTS': 'black',
-    'SelectBest'      : 'orange'
+    'SelectBest'      : 'orange',
+    'RF' : 'green',
+    'KNN' : 'purple',
+    'MLP' : 'gray'
+
 } 
 marker_dict = {
     'Baseline' : 's',
     'LuPTS'    : 'o',
     'Stat-LuPTS': 'D',
-    'SelectBest'      : 'p'
+    'SelectBest'      : 'p',
+    'RF' : '*',
+    'KNN' : 'X',
+    'MLP' : '^'
 }
 
 def method_color(method_name):
@@ -86,3 +94,35 @@ def set_mpl_default_settings():
     mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=['#377eb8', '#ff7f00', '#4daf4a', '#f781bf', '#a65628', '#984ea3', '#999999', '#e41a1c', '#dede00']) # Set the default color cycle
     mpl.rcParams['font.family'] = 'serif'
     mpl.rcParams['font.size'] = 18
+
+
+def print_table(dict_list, n_size, timestep=1):
+    """
+    Print out table for LaTeX
+    """
+    results_dict = {'Method': [model for model in dict_list[0]['model_list']]}
+    for city_dict in dict_list:
+        
+        city_results = []
+        n_list = city_dict['n_list']
+        city = city_dict['city']
+
+        assert n_size in n_list, "Missing desired sample size in data"
+
+        for idx, model in enumerate(city_dict['model_list']):
+            
+            assert model == results_dict['Method'][idx], "Incorrect ordering in results_dict entry for Methods"
+
+            for jdx, n in enumerate(n_list):
+                if int(n) == int(n_size):
+                    data_mean, data_std = city_dict['timestep'][timestep][model][jdx]
+                    city_results.append(f"{data_mean:0.2f} ({data_std:0.2f})")
+                    break
+        
+        results_dict[city.capitalize()] = city_results
+
+    df = pd.DataFrame(results_dict)
+
+    print(df.to_latex(index=False))
+
+    return df
